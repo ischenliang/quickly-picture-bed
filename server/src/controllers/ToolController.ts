9899172dcfce3224e71892e42178c10abe2dfc9d
itchenliang <itchenliang@163.com>
2023-04-06 09:16:45 +0800

    perf:优化

--- server/src/controllers/ToolController.ts
+++ server/src/controllers/ToolController.ts
@@ -301,35 +301,4 @@ class ToolController {
     //   sse(stream,'test',{a: "yango",b: Date.now()});
     // },3000); 
   }
-
-
-  
-
-  // 1.1版本
-  @Post('/chatgpt')
-  async chatgpt (@Body({ required: true }) params: { prompt: string, userId: string, network: boolean }) {
-    const importDynamic = new Function('modulePath', 'return import(modulePath)');
-    const { ChatGPTUnofficialProxyAPI, ChatGPTAPI } = await importDynamic("chatgpt");
-    const nodeFetch = await importDynamic('node-fetch')
-    const api = new ChatGPTAPI({
-      apiKey: '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',
-      // apiBaseUrl: 'https://service-isex76rq-1257144987.jp.apigw.tencentcs.com/'
-      fetch: (url: string, options = {}) => {
-        const defaultOptions = {
-          agent: proxy("http://127.0.0.1:3002"),
-        };
-        const mergedOptions = {
-          ...defaultOptions,
-          ...options,
-        };
-        return nodeFetch.default(url, mergedOptions);
-      },
-    })
-    let res = await api.sendMessage('东北锅包肉的家常做法')
-    return {
-      code: 200,
-      msg: '',
-      data: res
-    }
-  }
 }
