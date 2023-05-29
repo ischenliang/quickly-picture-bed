ed38635a5ae423f737584229ea08941ffec13d82
itchenliang <itchenliang@163.com>
2023-05-29 09:44:03 +0800

    fix:修复复制代码时会弹出多个复制成功提示问题

--- client/src/views/chatgpt/useCopyCode.ts
+++ client/src/views/chatgpt/useCopyCode.ts
@@ -5,18 +5,25 @@ export function useCopyCode(ctx) {
   function copyCodeBlock() {
     const codeBlockWrapper = document.querySelectorAll('.code-block-wrapper')
     codeBlockWrapper.forEach((wrapper) => {
-      const copyBtn = wrapper.querySelector('.code-block-header__copy')
-      const codeBlock = wrapper.querySelector('.code-block-body')
-      if (copyBtn && codeBlock) {
-        copyBtn.addEventListener('click', () => {
-          // if (navigator.clipboard?.writeText) {
-          //   navigator.clipboard.writeText(codeBlock.textContent ?? '')
-          // } else {
-          //   useCopyText(ctx, codeBlock.textContent ?? '')
-          // }
-          useCopyText(ctx, codeBlock.textContent ?? '')
-        })
-      }
+      const copyBtns = wrapper.querySelectorAll('.code-block-header__copy')
+      const codeBlocks = wrapper.querySelectorAll('.code-block-body')
+      copyBtns.forEach((copyBtn: HTMLElement, index) => {
+        if (copyBtn && codeBlocks[index]) {
+          copyBtn.onclick = () => {
+            useCopyText(ctx, codeBlocks[index].textContent ?? '')
+          }
+          // 不能使用 addEventListener 事件注册的方式，因为重复注册不会覆盖
+          // copyBtn.addEventListener('click', (e) => {
+          //   console.log(e)
+          //   // if (navigator.clipboard?.writeText) {
+          //   //   navigator.clipboard.writeText(codeBlock.textContent ?? '')
+          //   // } else {
+          //   //   useCopyText(ctx, codeBlock.textContent ?? '')
+          //   // }
+          //   useCopyText(ctx, codeBlocks[index].textContent ?? '')
+          // })
+        }
+      })
     })
   }
 
