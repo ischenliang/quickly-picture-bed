d2fddcdd6b8013afaf3ae3da735db8ed43791dfe
itchenliang <itchenliang@163.com>
2024-03-01 15:26:32 +0800

    feat:优化flex布局以及扩展vue-router的meta元信息类型定义

--- client/src/declare-typing.d.ts
+++ client/src/declare-typing.d.ts
@@ -1,4 +1,20 @@
+import 'vue-router'
+
+// 为路由元信息meta提供智能提示
+declare module 'vue-router' {
+  interface RouteMeta {
+    title: string // 标题
+    icon?: string // 图标
+    hidden?: boolean // 是否隐藏
+    active?: string // 激活菜单
+    role?: number[] // 该路由哪些角色可见
+    keepAlive?: boolean // 是否需要缓存
+  }
+}
+
+
 // 解决window.uploader_ip报警告问题
 interface Window {
   uploader_ip: string
 }
+
